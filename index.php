89c6772437b4fc2a42b7c155c864a7623bff1fdc
Mario <Mario-Duarte@users.noreply.github.com>
2016-07-10 16:47:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#Color Game</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  
+    <div class="particles-wrapper">
+      <div  class="particles floating-bubbles"></div>
+    </div>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.min.js"></script>
+  </body>
+</html>
